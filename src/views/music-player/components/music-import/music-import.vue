71bead2f6aacd80c9bc161fdcd16d728f0d8963c
xinyiliang <xinyiliang@yangqianguan.com>
2020-02-26 23:20:51 +0800

    basic import music

--- src/views/music-player/components/music-import/music-import.vue
+++ src/views/music-player/components/music-import/music-import.vue
@@ -1,13 +1,28 @@
 <template>
     <div>
         导入歌词
+        <input type="file" multiple @change="onSelect">
     </div>
 </template>
 
 <script>
 
     export default {
-        name: "MusicImport"
+        name: "MusicImport",
+
+        data() {
+            return {
+                musicList: [],
+                currMusic: null
+            };
+        },
+
+        methods: {
+            onSelect({target: {files}}) {
+                console.log('---- ', files);
+                this.$emit('importMusic', files);
+            }
+        }
     };
 
 </script>
